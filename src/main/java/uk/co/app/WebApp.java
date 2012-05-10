2728d2a43e8d9b505fef9c7eb5d0f1b0b7e4029b
Mike <mike.spelling@hotmail.co.uk>
2012-05-10 17:18:39 +0100

    Removed unused game endpoint and added placeholder for test/resources

--- src/main/java/uk/co/app/WebApp.java
+++ src/main/java/uk/co/app/WebApp.java
@@ -26,30 +26,5 @@ public class WebApp {
         mav.addObject("name", name);
         return mav;
     }
-
-	@RequestMapping("game")
-    public ModelAndView swear(ModelAndView mav) throws Exception {
-        mav = new ModelAndView("hello");
-        String app = "<br/>" +
-        		"<applet archive=\"Game.jar\" code=\"game/Game.class\" " +
-        		"width=\"1000\" height=\"800\">" +
-					"<param name=\"NumBalls\" value=\"2\" />" +
-					"<param name=\"Radius\" value=\"25\" />" +
-					"<param name=\"HeightMetres\" value=\"10\" />" +
-					"<param name=\"InitialVelocityX\" value=\"0\" />" +
-					"<param name=\"InitialVelocityY\" value=\"0\" />" +
-					"<param name=\"AccelerationX\" value=\"0\" />" +
-					"<param name=\"AccelerationY\" value=\"9.80665\" />" +
-					"<param name=\"EnergyLossLeft\" value=\"0.8\" />" +
-					"<param name=\"EnergyLossRight\" value=\"0.8\" />" +
-					"<param name=\"EnergyLossTop\" value=\"0.8\" />" +
-					"<param name=\"EnergyLossBottom\" value=\"0.8\" />" +
-					"<param name=\"EnergyLossCollision\" value=\"1\" />" +
-					"<param name=\"Circular\" value=\"true\" />" +
-					"You need to get Java!" +
-				"</applet>";
-        mav.addObject("name", app);
-        return mav;
-    }
 	
 }
